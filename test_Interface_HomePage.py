418bc3daa0a5c97f4d29d07223464dee2acd341c
Lucas Dejard <34687381+lucasdejard@users.noreply.github.com>
2022-09-15 17:31:12 -0300

    Avanço nos testes Críticos
    aumento da cobertura de testes, foco no registro e login

--- test_Interface_HomePage.py
+++ test_Interface_HomePage.py
@@ -6,8 +6,9 @@ from selenium.webdriver.common.by import By
 import time
 
 
-class Test_Contact:
+class TestContact:
     """Test the Link for ways to contact site"""
+
     def test_email(self):
         drivern = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
         drivern.get("http://testphp.vulnweb.com/login.php")
@@ -16,18 +17,76 @@ class Test_Contact:
         drivern.quit()
         return "True"
 
-    """Test the Link for E-mail"""
-    def test_number(self):
+    def test_abt(self):
         drivere = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
         drivere.get("http://testphp.vulnweb.com/login.php")
         clicknumbere = drivere.find_element(By.XPATH, '//a[normalize-space()="About Us"]').click()
-        time.sleep(6)
+        time.sleep(3)
         drivere.quit()
         return "True"
 
 
-class Test_login_false:
+class TestRegister:
+    """Test Register Page"""
+
+    def test_signup(self):
+        global drivern, clicknumbern
+        drivern = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+        drivern.get("http://testphp.vulnweb.com/login.php")
+        clicknumbern = drivern.find_element(By.XPATH, "//a[normalize-space()='signup here']").click()
+        time.sleep(3)
+        return "True"
+
+    def test_user(self):
+        clicknumbern = drivern.find_element(By.XPATH, "//input[@name='uuname']").send_keys("usernametest")
+        time.sleep(3)
+        return "True"
+
+    def test_pass(self):
+        clicknumbern = drivern.find_element(By.XPATH, "//input[@name='upass']").send_keys("passwordtest")
+        time.sleep(3)
+        return "True"
+
+    def test_pass_retype(self):
+        clicknumbern = drivern.find_element(By.XPATH, "//input[@name='upass2']").send_keys("passwordtest")
+        time.sleep(3)
+        return "True"
+
+    def test_name(self):
+        clicknumbern = drivern.find_element(By.XPATH, "//input[@name='urname']").send_keys("nametest")
+        time.sleep(3)
+        return "True"
+
+    def test_card(self):
+        clicknumbern = drivern.find_element(By.XPATH, "//input[@name='ucc']").send_keys("123876456098")
+        time.sleep(3)
+        return "True"
+
+    def test_email(self):
+        clicknumbern = drivern.find_element(By.XPATH, "//input[@name='uemail']").send_keys("emailtest@gmail.com")
+        time.sleep(3)
+        return "True"
+
+    def test_phone(self):
+        clicknumbern = drivern.find_element(By.XPATH, "//input[@name='uphone']").send_keys("40028922")
+        time.sleep(3)
+        return "True"
+
+    def test_address(self):
+        clicknumbern = drivern.find_element(By.XPATH, "//textarea[@name='uaddress']").send_keys("rua do fim do mundo, 000")
+        time.sleep(3)
+        return "True"
+
+    def test_login_click(self):
+        clicknumbern = drivern.find_element(By.XPATH, "//input[@name='signup']").click()
+        time.sleep(3)
+        drivern.quit()
+        return "True"
+
+
+class TestLoginFalse:
     """Test with wrong inputs to Fail"""
+
     def test_login_false(self):
         global drivern, clicknumbern
         drivern = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
@@ -39,5 +98,11 @@ class Test_login_false:
     def test_password_false(self):
         clicknumbern = drivern.find_element(By.XPATH, "//input[@name='pass']").send_keys("testefalha@gmail.com")
         time.sleep(3)
+        return "True"
+
+    def test_login_click(self):
+        clicknumbern = drivern.find_element(By.XPATH, "//input[@value='login']").click()
+        time.sleep(3)
         drivern.quit()
         return "True"
+
